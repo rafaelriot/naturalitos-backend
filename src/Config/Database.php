67f93071da0d040eee8fbe5e0641f755f7b3a097
Rafael <rafael.riot.o@gmail.com>
2026-09-06 20:52:41 -0600

    Support both DB_PASS and DB_PASSWORD variable names

--- src/Config/Database.php
+++ src/Config/Database.php
@@ -20,9 +20,9 @@ class Database
     {
         $host = $_ENV['DB_HOST'] ?? 'localhost';
         $port = $_ENV['DB_PORT'] ?? '3306';
-        $name = $_ENV['DB_NAME'] ?? 'naturalitos_movil_db';
-        $user = $_ENV['DB_USER'] ?? 'root';
-        $pass = $_ENV['DB_PASS'] ?? '';
+        $name = $_ENV['DB_NAME'] ?? ($_ENV['DB_DATABASE'] ?? 'naturalitos_movil_db');
+        $user = $_ENV['DB_USER'] ?? ($_ENV['DB_USERNAME'] ?? 'root');
+        $pass = $_ENV['DB_PASS'] ?? ($_ENV['DB_PASSWORD'] ?? '');
 
         $dsn = "mysql:host={$host};port={$port};dbname={$name};charset=utf8mb4";
 
